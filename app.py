a07851e9ab3b3f2c1a399220d9467aa2b7652690
Nikola Pavković <nikanberko286@gmail.com>
2022-07-03 17:21:24 +0200

    Changed port

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def download_file():
 	return send_file(path, as_attachment=True)
 
 if __name__=="__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
